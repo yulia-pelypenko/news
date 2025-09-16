77f98e200e65e08632ea05086e9a6b972f5a9ea3
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-09-16 14:36:30 +0300

    install and configure Vite plugins (svgr, checker, compression, inspect, visualizer, virtual) and Terser

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import svgr from "vite-plugin-svgr"
+import checker from "vite-plugin-checker"
+import compression from "vite-plugin-compression"
+import Inspect from "vite-plugin-inspect"
+import { visualizer } from "rollup-plugin-visualizer"
+import terser from "@rollup/plugin-terser"
+import virtual from "vite-plugin-virtual"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => ({
+  plugins: [
+    react(),
+    svgr(),
+    checker({ typescript: true }),
+    compression(),
+    Inspect(),
+    visualizer(),
+    virtual(),
+  ],
+  build: {
+    rollupOptions: {
+      plugins: [terser({
+        format: { comments: false },
+        compress: { drop_console: true, drop_debugger: true },
+      })],
+    },
+  },
+}))
+
