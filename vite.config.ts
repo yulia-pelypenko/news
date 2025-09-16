d022848775b6850eabcf6d0ce1c3e0b2aec3b699
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-09-16 15:22:51 +0300

    migrate from ESLint to Biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,29 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import svgr from "vite-plugin-svgr"
-import checker from "vite-plugin-checker"
-import compression from "vite-plugin-compression"
-import Inspect from "vite-plugin-inspect"
-import { visualizer } from "rollup-plugin-visualizer"
-import terser from "@rollup/plugin-terser"
+import terser from "@rollup/plugin-terser";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import compression from "vite-plugin-compression";
+import Inspect from "vite-plugin-inspect";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig(() => ({
-  plugins: [
-    react(),
-    svgr(),
-    checker({ typescript: true }),
-    compression(),
-    Inspect(),
-    visualizer(),
-  ],
-  build: {
-    rollupOptions: {
-      plugins: [terser({
-        format: { comments: false },
-        compress: { drop_console: true, drop_debugger: true },
-      })],
-    },
-  },
-}))
-
+	plugins: [
+		react(),
+		svgr(),
+		checker({ typescript: true }),
+		compression(),
+		Inspect(),
+		visualizer(),
+	],
+	build: {
+		rollupOptions: {
+			plugins: [
+				terser({
+					format: { comments: false },
+					compress: { drop_console: true, drop_debugger: true },
+				}),
+			],
+		},
+	},
+}));
