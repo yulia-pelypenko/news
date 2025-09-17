0589b150ccfb2bb6eeeeaa633435a11399e329d0
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-09-17 13:01:05 +0300

    add re-export  for auth components

---
+++ src/modules/auth/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as AuthNavigation } from "./AuthNavigation";
+export { default as AuthRoute } from "./AuthRoute";
+export { default as NoAuthRoute } from ".//NoAuthRoute";
+export { default as PublicRoute } from "./PublicRoute";
