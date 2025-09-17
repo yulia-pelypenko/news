746030b1fe8c7040b7fc05d10d1a2c865a5c0eb4
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-09-17 15:39:26 +0300

    add authModule with login and register routes

--- src/modules/common/interfaces/index.ts
+++ src/modules/common/interfaces/index.ts
@@ -1,12 +1,12 @@
-import type { RouteType } from "@/modules/auth/enums";
 import type { ComponentType } from "react";
+import type { RouteType } from "@/modules/auth/enums";
 
 export interface IModule {
-  routes: IRoute[];
+	routes: IRoute[];
 }
 
 export interface IRoute {
-  Component: ComponentType;
-  type: RouteType;
-  path: string;
+	Component: ComponentType;
+	type: RouteType;
+	path: string;
 }
