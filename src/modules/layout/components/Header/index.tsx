7d98b9c6132db9fc58aeb1863e6ef61d8f1fd90c
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-09-18 22:38:24 +0300

    wrapped Logo component with Link to AppRoutes.News

--- src/modules/layout/components/Header/index.tsx
+++ src/modules/layout/components/Header/index.tsx
@@ -1,11 +1,15 @@
 import type { FC } from "react";
+import { Link } from "react-router";
+import { Routes } from "@/config/routes";
 import { AuthNavigation } from "@/modules/auth/components";
 import { Logo, ThemeToggleButton } from "@/modules/common/components";
 
 const Header: FC = () => {
 	return (
 		<header className="w-full bg-white dark:bg-gray-900 shadow px-6 py-4 flex items-center justify-between">
-			<Logo />
+			<Link to={Routes.News}>
+				<Logo />
+			</Link>
 			<div className="flex items-center gap-4">
 				<AuthNavigation />
 				<ThemeToggleButton />
