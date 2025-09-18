3d02b422565b5d7b57928885a63cd136909aafb5
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-09-18 10:37:04 +0300

    connect modules routes in App with route type mapping

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,14 +1,12 @@
 import { authModule } from "./auth";
 import type { IModule, IRoute } from "./common/interfaces";
 
-const modules = [
-  authModule,
-];
+const modules = [authModule];
 
 const mergedModules: IModule = {
-  routes: modules.reduce((acc: IRoute[], { routes }) => {
-    return routes ? [...acc, ...routes] : acc;
-  }, []),
+	routes: modules.reduce((acc: IRoute[], { routes }) => {
+		return routes ? [...acc, ...routes] : acc;
+	}, []),
 };
 
 export default mergedModules;
