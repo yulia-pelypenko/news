ec9ba499664aa565d7e56aff085cfab3beddf024
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-09-18 14:24:19 +0300

    create ThemeToggleButton and add to header

--- src/modules/common/components/index.ts
+++ src/modules/common/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from "./Button";
 export { default as ControlledInput } from "./ControlledInput";
 export { default as Input } from "./Input";
 export { default as Logo } from "./Logo";
+export { default as ThemeToggleButton } from "./ThemeToggleButton";
--- src/modules/layout/components/Header/index.tsx
+++ src/modules/layout/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import type { FC } from "react";
 import { AuthNavigation } from "@/modules/auth/components";
-import { Logo } from "@/modules/common/components";
+import { Logo, ThemeToggleButton } from "@/modules/common/components";
 
 const Header: FC = () => {
 	return (
 		<header className="w-full bg-white dark:bg-gray-900 shadow px-6 py-4 flex items-center justify-between">
 			<Logo />
-			<AuthNavigation />
+			<div className="flex items-center gap-4">
+				<AuthNavigation />
+				<ThemeToggleButton />
+			</div>
 		</header>
 	);
 };
