59dc5685714a7a01c84cacf101cf46ae9e1b2f7d
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-09-25 11:20:40 +0300

    add slots fo ads

--- src/modules/layout/components/MainLayout/index.tsx
+++ src/modules/layout/components/MainLayout/index.tsx
@@ -10,7 +10,17 @@ const MainLayout: FC<Props> = ({ children }) => {
 		<div className="flex min-h-screen flex-col">
 			<div className="mx-auto flex w-full max-w-[1280px] bg-gray-100 dark:bg-gray-950 flex-1 flex-col">
 				<Header />
-				<main className="flex-1 py-6">{children}</main>
+				<div className="flex justify-center gap-4 py-4">
+					<iframe
+						id="add-frame-left"
+						className="flex justify-center items-center border border-dashed border-gray-400 w-[320px] h-[270px] overflow-hidden"
+					/>
+					<iframe
+						id="add-frame-right"
+						className="flex justify-center items-center border border-dashed border-gray-400 w-[320px] h-[270px] overflow-hidden"
+					/>
+				</div>
+				<main className="flex-1">{children}</main>
 				<Footer />
 			</div>
 		</div>
