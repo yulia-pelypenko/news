406345ca935f481bcb4e5c51dcd7569da8ac73d4
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-09-30 15:14:00 +0300

    move ads to AdSlot in MainLayout

--- src/modules/layout/components/MainLayout/index.tsx
+++ src/modules/layout/components/MainLayout/index.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Footer, Header } from "@/modules/layout/components";
+import AdSlot from "@/modules/prebid/components/AdSlot";
 
 const MainLayout = () => {
 	return (
@@ -7,16 +8,8 @@ const MainLayout = () => {
 			<div className="mx-auto flex w-full max-w-[1280px] bg-gray-100 dark:bg-gray-950 flex-1 flex-col">
 				<Header />
 				<div className="flex justify-center gap-4 py-4">
-					<iframe
-						title="Advertisement slot left"
-						id="add-frame-left"
-						className="flex justify-center items-center border border-dashed border-gray-400 w-[320px] h-[270px] overflow-hidden"
-					/>
-					<iframe
-						title="Advertisement slot right"
-						id="add-frame-right"
-						className="flex justify-center items-center border border-dashed border-gray-400 w-[320px] h-[270px] overflow-hidden"
-					/>
+					<AdSlot id="add-frame-left" />
+					<AdSlot id="add-frame-right" />
 				</div>
 				<main className="flex-1">{<Outlet />}</main>
 				<Footer />
